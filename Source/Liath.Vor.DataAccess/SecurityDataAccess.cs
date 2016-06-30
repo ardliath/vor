8cf52fe0f823285f90806c72cbb8a027caee019a
ardliath <ardliath@gmail.com>
2016-06-30 21:05:50 +0100

    Started adding classes

---
+++ Source/Liath.Vor.DataAccess/SecurityDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Liath.Vor.Contracts.DataAccess;
+using Liath.Vor.Models;
+
+namespace Liath.Vor.DataAccess
+{
+  public class SecurityDataAccess : ISecurityDataAccess
+  {
+    public UserAccount GetOrCreateUserAccount(string domainName)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ Source/Liath.Vor.Models/UserAccount.cs
@@ -0,0 +1,10 @@
+﻿namespace Liath.Vor.Models
+{
+  public class UserAccount
+  {
+    public int? UserAccountID { get; set; }
+    public string DomainName { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+  }
+}
