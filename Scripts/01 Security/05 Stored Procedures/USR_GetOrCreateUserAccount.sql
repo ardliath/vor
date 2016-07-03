2e17ee18b2267a6f9012b2c8c0ce28bb81ff6f7c
ardliath <ardliath@gmail.com>
2016-07-03 13:23:23 +0100

    Created the SP USR_GetOrCreateUser

---
+++ Scripts/01 Security/05 Stored Procedures/USR_GetOrCreateUserAccount.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE USR_GetOrCreateUser
+	@DomainName nvarchar(255)
+AS
+
+IF NOT EXISTS(SELECT * FROM UserAccounts WHERE DomainName = @DomainName)
+BEGIN
+	INSERT INTO UserAccounts(DomainName) VALUES (@DomainName)
+END
+
+SELECT UserAccountID, DomainName, Firstname, Lastname
+FROM UserAccounts
+WHERE DomainName = @DomainName
+
