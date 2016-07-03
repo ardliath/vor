e2ae43831f11181f992bdb84aebeadec176d1834
ardliath <ardliath@gmail.com>
2016-07-03 13:30:20 +0100

    Created the data access command

--- Source/Liath.Vor.DataAccess/SecurityDataAccess.cs
+++ Source/Liath.Vor.DataAccess/SecurityDataAccess.cs
@@ -1,18 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Liath.Vor.Contracts.DataAccess;
 using Liath.Vor.Models;
+using Liath.Vor.Session;
 
 namespace Liath.Vor.DataAccess
 {
   public class SecurityDataAccess : ISecurityDataAccess
   {
+    private readonly ISessionManager _sessionManager;
+    public SecurityDataAccess(ISessionManager sessionManager)
+    {
+      if (sessionManager == null) throw new ArgumentNullException(nameof(sessionManager));
+      _sessionManager = sessionManager;
+    }
+
     public UserAccount GetOrCreateUserAccount(string domainName)
     {
-      throw new NotImplementedException();
+      using (var cmd = _sessionManager.CreateUnitOfWork().CreateCommand("USR_GetOrCreateUser"))
+      {
+        cmd.CommandType = CommandType.StoredProcedure;
+        var param = cmd.CreateParameter();
+        param.ParameterName = "DomainName";
+        param.Value = domainName;
+        cmd.Parameters.Add(param);
+
+        using (var dr = cmd.ExecuteReader())
+        {
+          if (dr.Read())
+          {
+            var user = new UserAccount();
+            user.UserAccountID = dr.GetInt32(0);
+            user.DomainName = dr.GetString(1);
+            user.Firstname = dr.IsDBNull(2) ? null : dr.GetString(2);
+            user.Lastname = dr.IsDBNull(3) ? null : dr.GetString(3);
+
+            return user;
+          }
+        }
+      }
+
+      return null;
     }
   }
 }
