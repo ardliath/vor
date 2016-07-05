bb27f99d655740f6ae43a1402adb3cb4ee259f64
ardliath <ardliath@gmail.com>
2016-07-05 17:05:54 +0100

    Added Next/Back buttons and disabled when it's at the first question
    Included underscore.js

--- Source/Liath.Vor.UI.Web/Scripts/Apps/Quiz/Controllers/QuestionController.js
+++ Source/Liath.Vor.UI.Web/Scripts/Apps/Quiz/Controllers/QuestionController.js
@@ -1,14 +1,23 @@
 ﻿quizApp.controller("questionController", function ($scope, $http, $log) {  
 
   $scope.exam = {};
+  $scope.buttons = {
+    backButtonDisabled: true
+  };
+
 
   $scope.getExam = function(id) {
     $http.get("../../Question/Exam/" + id)
 			.success(function (response) {
 			  $scope.exam = response;
+        $scope.setButtonState();
       })
 			.error(function (data, status, headers, config) {
 			  $log.error(data, status, headers, config);
 			});
+  }
+
+  $scope.setButtonState = function() {
+    $scope.buttons.backButtonDisabled = _.first($scope.exam.Quiz.Questions).QuestionID === $scope.exam.CurrentQuestionID;
   }
 });
