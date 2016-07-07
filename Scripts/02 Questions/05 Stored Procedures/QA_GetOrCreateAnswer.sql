b69b4377130b07f4f9fb3f529a5cd0a7efc24f0f
ardliath <ardliath@gmail.com>
2016-07-07 08:11:51 +0100

    Updated QA_GetOrCreateAnswer  to return selected options

--- Scripts/02 Questions/05 Stored Procedures/QA_GetOrCreateAnswer.sql
+++ Scripts/02 Questions/05 Stored Procedures/QA_GetOrCreateAnswer.sql
@@ -8,3 +8,7 @@ AS
 	END
 
 SELECT AnswerID, ExamID, QuestionID FROM Answers WHERE QuestionID = @QuestionID AND ExamID = @ExamID
+
+SELECT ao.AnswerID, ao.OptionID, ao.QuestionID FROM AnswerOptions ao
+JOIN Answers a on ao.AnswerID = a.AnswerID
+WHERE a.QuestionID = @QuestionID AND a.ExamID = @ExamID
