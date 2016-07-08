aad9e2b4700bd264cce869188ac8907c7f3c1b42
ardliath <ardliath@gmail.com>
2016-07-08 16:27:06 +0100

    Updated GetOrCreateExam to return answers already submitted

--- Scripts/02 Questions/05 Stored Procedures/QA_GetOrCreateExam.sql
+++ Scripts/02 Questions/05 Stored Procedures/QA_GetOrCreateExam.sql
@@ -13,3 +13,10 @@ END
 SELECT ExamID, QuizID, UserAccountID, QuestionID, StartDate, EndDate, Score
 FROM Exams
 WHERE QuizID = @QuizID AND UserAccountID = @UserAccountID AND EndDate IS NULL
+
+
+SELECT ao.AnswerID, ao.QuestionID, ao.OptionID
+FROM AnswerOptions ao
+JOIN Answers a on ao.AnswerID = a.AnswerID
+JOIN Exams e on a.ExamID = e.ExamID
+AND QuizID = @QuizID AND UserAccountID = @UserAccountID AND EndDate IS NULL
