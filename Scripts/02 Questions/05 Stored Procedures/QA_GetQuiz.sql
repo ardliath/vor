01a798bbb89a5e8fc9954639b468c24a31cecc5d
ardliath <ardliath@gmail.com>
2016-07-11 15:45:16 +0100

    Fixed GetQuiz SP

--- Scripts/02 Questions/05 Stored Procedures/QA_GetQuiz.sql
+++ Scripts/02 Questions/05 Stored Procedures/QA_GetQuiz.sql
@@ -1,13 +1,6 @@
-CREATE PROCEDURE QA_GetQuestion
-	@QuestionID int
+CREATE PROCEDURE QA_GetQuiz
+	@QuizID int
 AS
-SELECT QuestionID, Text, Type From Questions where QuestionID = @QuestionID
-
-SELECT UserAccountID, DomainName, Firstname, Lastname
-FROM UserAccounts u
-JOIN Questions q on q.CreatedBy = u.UserAccountID and q.QuestionID = @QuestionID
-
-SELECT o.OptionID, o.Text, o.IsCorrect
-FROM Options o
-JOIN Questions q on q.QuestionID = o.QuestionID and q.QuestionID = @QuestionID
-ORDER BY Ordinal
+SELECT QuizID, Title, StartDate, EndDate
+FROM Quiz
+WHERE QuizID = @QuizID
