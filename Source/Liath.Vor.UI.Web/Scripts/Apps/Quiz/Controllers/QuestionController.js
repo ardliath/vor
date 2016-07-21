f438ed35987f6d41b840f6b2e213882305f021b0
ardliath <ardliath@gmail.com>
2016-07-21 15:50:20 +0100

    Answers are pre-populated when returning to exam

--- Source/Liath.Vor.UI.Web/Scripts/Apps/Quiz/Controllers/QuestionController.js
+++ Source/Liath.Vor.UI.Web/Scripts/Apps/Quiz/Controllers/QuestionController.js
@@ -1,6 +1,6 @@
 ﻿quizApp.controller("questionController", function ($scope, $http, $log) {
 
-  $scope.exam = {};
+  $scope.thisExam = {};
   $scope.buttons = {
     backButtonDisabled: true
   };
@@ -9,7 +9,7 @@
   $scope.getExam = function (id) {
     $http.get("../../Question/Exam/" + id)
 			.success(function (response) {
-			  $scope.exam = response;
+			  $scope.createExamFromResponse(response);
 			  $scope.setButtonState();
 			})
 			.error(function (data, status, headers, config) {
@@ -17,8 +17,54 @@
 			});
   }
 
+  $scope.createExamFromResponse = function (response) {    
+    $scope.thisExam = response.Quiz;
+    $scope.thisExam.examID = response.ExamID;
+    $scope.thisExam.currentQuestionID = response.CurrentQuestionID;
+
+    _.each($scope.thisExam.Questions, function (question) {
+      if (question.Type === 0) { // if it's exclusive
+        question.selectedOption = $scope.getExclusiveSelectedOption(question, response.Answers);
+      } else {
+        _.each(question.Options, function(option) {
+          option.selected = $scope.isOptionSelected(question, option, response.Answers);
+        });
+      }
+    });
+  }
+
+  $scope.getExclusiveSelectedOption = function (thisQuestion, allAnswers) {
+
+    var val = -1;
+    _.each(allAnswers, function (answer) {
+      if (thisQuestion.QuestionID === answer.QuestionID) {
+        _.each(answer.AnswerOptions, function (selectedOption) {
+          val = selectedOption.OptionID;
+        });
+      }
+    });
+
+    return val;
+  }
+
+  $scope.isOptionSelected = function (thisQuestion, thisOption, allAnswers) {
+
+    var val = false;
+    _.each(allAnswers, function(answer) {
+      if (thisQuestion.QuestionID === answer.QuestionID) {
+        _.each(answer.AnswerOptions, function (selectedOption) {
+          if (selectedOption.OptionID === thisOption.OptionID) {
+            val = true;
+          }
+        });
+      }
+    });
+
+    return val;
+  }
+
   $scope.setButtonState = function () {
-    $scope.buttons.backButtonDisabled = _.first($scope.exam.Quiz.Questions).QuestionID === $scope.exam.CurrentQuestionID;
+    $scope.buttons.backButtonDisabled = _.first($scope.thisExam.Questions).QuestionID === $scope.thisExam.currentQuestionID;
   }
 
   $scope.nextQuestion = function () {
@@ -28,24 +74,28 @@
   $scope.submitAnswer = function (isForwards) {
 
     var selectedOptions = new Array();
-    _.each($scope.exam.Quiz.Questions, function(question) {
-      if (question.QuestionID === $scope.exam.CurrentQuestionID) {
-        _.each(question.Options, function(option) {
-          if (option.selected) {
-            selectedOptions.push(option.OptionID);
-          }
-        });
+    _.each($scope.thisExam.Questions, function(question) {
+      if (question.QuestionID === $scope.thisExam.currentQuestionID) {
+        if (question.Type === 0) { // exlusive
+          selectedOptions.push(question.selectedOption);
+        } else {
+          _.each(question.Options, function(option) {
+            if (option.selected) {
+              selectedOptions.push(option.OptionID);
+            }
+          });
+        }
       }
     });
 
     $http.post("../../Question/Answer", {
-      ExamID: $scope.exam.ExamID,
-      QuestionID: $scope.exam.CurrentQuestionID,
+      ExamID: $scope.thisExam.examID,
+      QuestionID: $scope.thisExam.currentQuestionID,
       IsForwards: isForwards,
       Options: selectedOptions
     })
 			.success(function (response) {
-        $scope.exam.CurrentQuestionID = response.NextQuestionID;
+			  $scope.thisExam.currentQuestionID = response.NextQuestionID;
 			  $scope.setButtonState();
 			})
 			.error(function (data, status, headers, config) {
